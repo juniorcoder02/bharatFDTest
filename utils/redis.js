00d8f54eee536e093a1b00ae682310408ae5dbaf
juniorcoder02 <132187577+juniorcoder02@users.noreply.github.com>
2025-02-02 01:50:54 +0530

    fix: Improve translation caching

--- utils/redis.js
+++ utils/redis.js
@@ -1,6 +1,8 @@
 const redis = require("redis");
 const client = redis.createClient();
 
+client.on("connect", () => console.log("Connected to Redis"));
+
 client.on("error", (err) => console.error("Redis Error:", err));
 
 module.exports = {
